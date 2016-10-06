d5edca0d3c026fbec00d411b4e3afa3d8aa3354a
Omar Gonzalez <ogonzalez@perceptivatech.com>
2016-10-06 11:53:53 -0300

    navegador-posta1.0

--- firstVaadinApp/src/main/java/com/perceptiva/test/FirstDesignImpl.java
+++ firstVaadinApp/src/main/java/com/perceptiva/test/FirstDesignImpl.java
@@ -1,10 +1,12 @@
 package com.perceptiva.test;
 
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Label;
 
-public class FirstDesignImpl extends firstDesign {
+public class FirstDesignImpl extends firstDesign implements View {
 
 	private static final long serialVersionUID = 1L;
 	
@@ -17,6 +19,12 @@ public class FirstDesignImpl extends firstDesign {
 				
 			}
 		});
+	}
+
+	@Override
+	public void enter(ViewChangeEvent event) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
