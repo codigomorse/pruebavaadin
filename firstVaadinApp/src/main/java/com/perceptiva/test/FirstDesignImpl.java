39d055746a0009d50dd96b1081e15b059be8240d
Omar Gonzalez <ogonzalez@perceptivatech.com>
2016-10-07 12:46:18 -0300

    navegando

--- firstVaadinApp/src/main/java/com/perceptiva/test/FirstDesignImpl.java
+++ firstVaadinApp/src/main/java/com/perceptiva/test/FirstDesignImpl.java
@@ -1,5 +1,11 @@
 package com.perceptiva.test;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Button.ClickEvent;
@@ -9,6 +15,7 @@ import com.vaadin.ui.Label;
 public class FirstDesignImpl extends firstDesign implements View {
 
 	private static final long serialVersionUID = 1L;
+
 	
 	public FirstDesignImpl() {
 		btn1.addClickListener(new ClickListener() {
@@ -19,12 +26,31 @@ public class FirstDesignImpl extends firstDesign implements View {
 				
 			}
 		});
+		btnWolverine.addClickListener(new ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				try {
+					Desktop.getDesktop().browse(new URL("http://stackoverflow.com/questions/10967451/open-a-link-in-browser-with-java-button").toURI());
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (URISyntaxException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+			
+			}
+		});
 	}
 
 	@Override
 	public void enter(ViewChangeEvent event) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
